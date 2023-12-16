cac0c10bffa7bd60243e46d662c00f27e8f3f343
Boshan Chen <boshanchen@Boshans-MacBook-Air.local>
2023-12-15 17:19:37 -0800

    completed first part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,8 +1,17 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Boshan Chen
  */
 public class LeapYear {
+    /** Judge a year is a leap year or not
+     *  @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
 
+        return false;
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
